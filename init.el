536e6956846d3cc568b193a4dcecea60939157fc
Lincoln Lee <git@lincoln.hk>
2015-10-21 02:59:33 +0000

    Add darktooth-theme and ace-jump-mode

--- init.el
+++ init.el
@@ -37,21 +37,16 @@
 (use-package mac-key-mode
   :defer t
   :config
-   (mac-key-mode 1))
+  (mac-key-mode 1))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (darktooth)))
- '(custom-safe-themes
-   (quote
-    ("00f33c1c844d52ea581f6e6eefb00d44bfc0ce8c69da8d302cf6fdbc279267b5" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(use-package ace-jump-mode
+  :bind ("C-c SPC" . ace-jump-mode)
+  :ensure t)
+
+(use-package darktooth-theme
+  :config
+  (load-theme 'darktooth t)
+  :ensure t)
+
+
 
