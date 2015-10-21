0aaa9bbd224ad9e0cd604f3f1125dcd4446bbfde
Lincoln Lee <git@lincoln.hk>
2015-10-21 02:59:06 +0000

    Add package repository

--- init.el
+++ init.el
@@ -1,4 +1,8 @@
 (require 'package)
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.org/packages/")))
+
 
 (package-initialize)
 
