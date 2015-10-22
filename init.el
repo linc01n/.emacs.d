ff339af382d990051853cc4ddf8bbc973867e308
Lincoln Lee <git@lincoln.hk>
2015-10-22 22:56:13 +0800

    Replace use-package with req-package

--- init.el
+++ init.el
@@ -21,86 +21,87 @@
   :ensure t)
 
 ;; TODO: Break each package init into a separate file
-(use-package magit
+(req-package magit
   :bind ("M-`" . magit-status)
   :config
   (setq magit-push-always-verify nil)
   (setq magit-commit-ask-to-stage f)
-  :ensure t)
+  )
 
-(use-package ibuffer
+(req-package ibuffer
   :bind ("<f14>" . ibuffer))
 
-(use-package switch-window
+(req-package switch-window
   :bind (("S-'" . switch-window)
          ("C-x o" . switch-window))
-  :ensure t)
+  )
 
-(use-package ace-jump-mode
+(req-package ace-jump-mode
   :bind ("C-c SPC" . ace-jump-mode)
-  :ensure t)
+  )
 
-(use-package ag
+(req-package ag
   :bind ("C-x g" . ag)
-  :ensure t)
+  )
 
-(use-package hc-zenburn-theme
+(req-package hc-zenburn-theme
   :defer t
   :disabled t
-  :ensure t)
+  )
 
-(use-package darktooth-theme
+(req-package darktooth-theme
   :config
   (load-theme 'darktooth t)
-  :ensure t)
+  )
 
-(use-package whitespace-cleanup-mode
+(req-package whitespace-cleanup-mode
   :defer t
   :config
   (global-whitespace-cleanup-mode 1)
   (add-hook 'before-save-hook 'whitespace-cleanup)
-  :ensure t)
+  )
 
-(use-package smex
+(req-package smex
   :bind ("M-x" . smex)
   :config
   (smex-initialize)
-  :ensure t)
+  )
 
-(use-package flx-ido
-  :defer t
-  :ensure t)
+(req-package flx-ido
+  :require ido
+  )
 
-(use-package ido-ubiquitous
-  :defer t
+(req-package ido-ubiquitous
+  :require ido
   :config
     (ido-ubiquitous-mode 1)
+  )
 
-  :ensure t)
-
-(use-package ido
-  :defer t
+(req-package ido
+  :require ido-vertical-mode
   :config
   (setq ido-create-new-buffer 'always)
   (setq ido-enable-flex-matching t)
   (setq ido-everywhere t)
   (ido-mode 1)
   (flx-ido-mode 1)
-  :ensure t)
+  )
 
-(use-package ido-vertical-mode
-  :defer t
+(req-package ido-vertical-mode
   :config
   (ido-vertical-mode 1)
   (setq ido-vertical-define-keys 'C-n-C-p-up-down-left-right)
-  :ensure t)
+  )
 
-(use-package occur-x
+(req-package occur-x
   :defer t
   :config
   (add-hook 'occur-mode-hook 'turn-on-occur-x-mode)
-  :ensure t)
+  )
 
-(use-package iedit
-  :bind ("C-c ;" . iedit-mode)
-  :ensure t)
+(req-package iedit
+  :bind ("C-;" . iedit-mode)
+  )
+
+
+(req-package-finish)
