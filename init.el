a217bf1b35237550af3e388249e175b08e8ada24
Lincoln Lee <git@lincoln.hk>
2015-10-23 04:43:36 +0000

    Remove marmalade

--- init.el
+++ init.el
@@ -1,6 +1,5 @@
 (require 'package)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "https://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.org/packages/")))
 
 
