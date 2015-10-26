465d77556cd01afdda3dbf8fa692c7d5ca91fe3e
Lincoln Lee <git@lincoln.hk>
2015-10-26 23:34:33 +0800

    Set git executable path in mac

--- init.el
+++ init.el
@@ -34,6 +34,9 @@
 ;; TODO: Break each package init into a separate file
 (req-package magit
   :bind ("M-`" . magit-status)
+  :init
+  (when (eq system-type 'darwin) ;; mac specific settings
+    (setq magit-git-executable "/usr/local/bin/git"))
   :config
   (setq magit-push-always-verify nil)
   (setq magit-commit-ask-to-stage f))
