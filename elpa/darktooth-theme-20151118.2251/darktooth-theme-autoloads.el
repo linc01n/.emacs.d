06721180d6c4e5ea5a613b72fa49866d3c650af1
Lincoln Lee <git@lincoln.hk>
2015-11-20 19:25:51 +0800

    [Updating darktooth-theme version: 20151118.2251][removing old version: 20151104.1818]

--- elpa/darktooth-theme-20151118.2251/darktooth-theme-autoloads.el
+++ elpa/darktooth-theme-20151118.2251/darktooth-theme-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "darktooth-theme" "darktooth-theme.el" (22087
-;;;;;;  16548 0 0))
+;;;### (autoloads nil "darktooth-theme" "darktooth-theme.el" (22095
+;;;;;;  813 0 0))
 ;;; Generated autoloads from darktooth-theme.el
 
 (and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
