31caa94a1a5419121b16b492a29cc062b2ba4f43
Lincoln Lee <git@lincoln.hk>
2015-11-26 02:56:46 +0000

    [Updating exec-path-from-shell version: 20151125.1133][removing old version: 20151121.1359]

--- elpa/exec-path-from-shell-20151125.1133/exec-path-from-shell-autoloads.el
+++ elpa/exec-path-from-shell-20151125.1133/exec-path-from-shell-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
 ;;;### (autoloads nil "exec-path-from-shell" "exec-path-from-shell.el"
-;;;;;;  (22097 25463 0 0))
+;;;;;;  (22102 29904 829045 400000))
 ;;; Generated autoloads from exec-path-from-shell.el
 
 (autoload 'exec-path-from-shell-copy-envs "exec-path-from-shell" "\
