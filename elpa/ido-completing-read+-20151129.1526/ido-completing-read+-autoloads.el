e89ce82c5ce0afc88a4b63eea734cc61622daaa0
Lincoln Lee <git@lincoln.hk>
2015-11-30 17:04:22 +0800

    [Updating ido-completing-read+ version: 20151129.1526][removing old version: 20151124.853]

--- elpa/ido-completing-read+-20151129.1526/ido-completing-read+-autoloads.el
+++ elpa/ido-completing-read+-20151129.1526/ido-completing-read+-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
 ;;;### (autoloads nil "ido-completing-read+" "ido-completing-read+.el"
-;;;;;;  (22106 21541 198891 927000))
+;;;;;;  (22108 4143 0 0))
 ;;; Generated autoloads from ido-completing-read+.el
 
 (defvar ido-cr+-enable-next-call nil "\
