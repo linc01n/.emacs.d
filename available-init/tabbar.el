6556b9af40a0577abdcc30949a0c1e42d36b3e6f
Lincoln Lee <git@lincoln.hk>
2015-12-05 20:37:22 +0800

    Group tab by project

--- available-init/tabbar.el
+++ available-init/tabbar.el
@@ -38,8 +38,7 @@
              (custom-set-variables '(tabbar-separator (quote (0.5))))
              ;; adding spaces
              (defun tabbar-buffer-tab-label (tab)
-               "Return a label for TAB.
-             That is, a string used to represent it on the tab bar."
+               "Return a label for TAB.That is, a string used to represent it on the tab bar."
                (let ((label (if tabbar--buffer-show-groups
                                 (format "[%s]  "
                                         (tabbar-tab-tabset tab))
@@ -56,8 +55,34 @@
                                         (/ (window-width)
                                            (length (tabbar-view (tabbar-current-tabset)))))))))
 
+             ;; a function to aid in finding the project root directory
+             (defun my-project-root ()
+               "Return the root of the project."
+               (locate-dominating-file default-directory
+                                       ".git"))
+
+             ;; Set the tabgroup based on project; Needs find-files-in-project
+             (setq tabbar-buffer-groups-function
+                   (lambda ()
+                     (let ((dir (expand-file-name default-directory)))
+                       (cond ((member (buffer-name) '("*Completions*"
+                                                      "*scratch*"
+                                                      "*Messages*"
+                                                      "*Ediff Registry*"))
+                              (list "#misc"))
+                             ;; All Magit status goes the same place
+                             ((string-match "^*magit" (buffer-name)) (list "#magic"))
+                             ((string-match "^COMMIT_EDITMSG" (buffer-name)) (list "#magic"))
+                             ;; All Cider windows
+                             ((string-match "^*nrepl-server" (buffer-name)) (list "#cider"))
+                             ((string-match "^*cider" (buffer-name)) (list "#cider"))
+                             ;; Group tabs based on project root
+                             ((my-project-root) (list (my-project-root)))
+                             ;; Use the current dir
+                             (t (list dir))))))
              (tabbar-mode 1)
              (tabbar-local-mode 1)
+             :demand t
              :bind
              (("s-<left>" . tabbar-backward-tab)
               ("s-<right>" . tabbar-forward-tab)))
