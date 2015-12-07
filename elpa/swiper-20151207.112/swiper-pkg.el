67670caa48f5bc4cbbd08ae9bad39a274b031ca2
Lincoln Lee <git@lincoln.hk>
2015-12-07 21:45:51 +0800

    [Updating swiper version: 20151207.112][removing old version: 20151204.23]

--- elpa/swiper-20151207.112/swiper-pkg.el
+++ elpa/swiper-20151207.112/swiper-pkg.el
@@ -1,4 +1,4 @@
-(define-package "swiper" "20151204.23" "Isearch with an overview. Oh, man!"
+(define-package "swiper" "20151207.112" "Isearch with an overview. Oh, man!"
   '((emacs "24.1"))
   :url "https://github.com/abo-abo/swiper" :keywords
   '("matching"))
