926a36e724dd64c92dd1cf030b2399b99967038d
Lincoln Lee <git@lincoln.hk>
2015-12-07 06:17:49 +0000

    [Updating visual-regexp version: 20151206.519][removing old version: 20151125.1129]

--- elpa/visual-regexp-20151206.519/visual-regexp-autoloads.el
+++ elpa/visual-regexp-20151206.519/visual-regexp-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "visual-regexp" "visual-regexp.el" (22105 21642
-;;;;;;  0 0))
+;;;### (autoloads nil "visual-regexp" "visual-regexp.el" (22117 9320
+;;;;;;  169192 139000))
 ;;; Generated autoloads from visual-regexp.el
 
 (autoload 'vr/mc-mark "visual-regexp" "\
