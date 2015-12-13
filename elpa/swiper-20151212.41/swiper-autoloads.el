e7cb9ca837091c1aedbe2b9fa519e5024a157960
Lincoln Lee <git@lincoln.hk>
2015-12-13 16:07:46 +0800

    [Updating swiper version: 20151212.41][removing old version: 20151209.115]

--- elpa/swiper-20151212.41/swiper-autoloads.el
+++ elpa/swiper-20151212.41/swiper-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "ivy" "ivy.el" (22120 9996 0 0))
+;;;### (autoloads nil "ivy" "ivy.el" (22125 10007 0 0))
 ;;; Generated autoloads from ivy.el
 
 (autoload 'ivy-resume "ivy" "\
@@ -63,7 +63,7 @@ Find a file on `recentf-list'.
 
 ;;;***
 
-;;;### (autoloads nil "swiper" "swiper.el" (22120 9996 0 0))
+;;;### (autoloads nil "swiper" "swiper.el" (22125 10007 0 0))
 ;;; Generated autoloads from swiper.el
 
 (autoload 'swiper-avy "swiper" "\
@@ -85,7 +85,7 @@ When non-nil, INITIAL-INPUT is the initial search pattern.
 ;;;***
 
 ;;;### (autoloads nil nil ("colir.el" "ivy-hydra.el" "swiper-pkg.el")
-;;;;;;  (22120 9996 991802 0))
+;;;;;;  (22125 10007 248903 0))
 
 ;;;***
 
