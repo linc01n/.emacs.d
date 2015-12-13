39fa31767d020a898c0eb639c7bedb9b8fa04348
Lincoln Lee <git@lincoln.hk>
2015-12-13 16:07:46 +0800

    [Updating smex version: 20151212.1409][removing old version: 20150822.1146]

--- elpa/smex-20151212.1409/smex-autoloads.el
+++ elpa/smex-20151212.1409/smex-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "smex" "smex.el" (22087 16586 0 0))
+;;;### (autoloads nil "smex" "smex.el" (22125 10008 0 0))
 ;;; Generated autoloads from smex.el
 
 (autoload 'smex "smex" "\
