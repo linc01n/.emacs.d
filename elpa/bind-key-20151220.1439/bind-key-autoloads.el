c5089651b650c7b54fea7b94269dc6279a7cd10d
Lincoln Lee <git@lincoln.hk>
2015-12-21 19:36:09 +0800

    [Updating bind-key version: 20151220.1439][removing old version: 20150321.213]

--- elpa/bind-key-20151220.1439/bind-key-autoloads.el
+++ elpa/bind-key-20151220.1439/bind-key-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "bind-key" "bind-key.el" (22087 16541 0 0))
+;;;### (autoloads nil "bind-key" "bind-key.el" (22135 58390 0 0))
 ;;; Generated autoloads from bind-key.el
 
 (autoload 'bind-key "bind-key" "\
