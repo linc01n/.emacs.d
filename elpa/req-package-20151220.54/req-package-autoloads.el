4e54eddbadd925484f4dd3d2e2352c924b63eee4
Lincoln Lee <git@lincoln.hk>
2015-12-21 19:36:09 +0800

    [Updating req-package version: 20151220.54][removing old version: 20151205.842]

--- elpa/req-package-20151220.54/req-package-autoloads.el
+++ elpa/req-package-20151220.54/req-package-autoloads.el
@@ -3,6 +3,12 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
+;;;### (autoloads nil nil ("req-package-args.el" "req-package-cycles.el"
+;;;;;;  "req-package-hooks.el" "req-package-pkg.el" "req-package-providers.el"
+;;;;;;  "req-package.el") (22135 58378 111464 0))
+
+;;;***
+
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
