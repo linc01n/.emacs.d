9da7993624c3a0f7cb051c1bcd2723eceeca2f55
Lincoln Lee <git@lincoln.hk>
2015-12-27 14:36:51 +0800

    [Updating ag version: 20151226.1605][removing old version: 20151219.903]

--- elpa/ag-20151226.1605/ag-autoloads.el
+++ elpa/ag-20151226.1605/ag-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "ag" "ag.el" (22134 45837 0 0))
+;;;### (autoloads nil "ag" "ag.el" (22143 34505 0 0))
 ;;; Generated autoloads from ag.el
 
 (autoload 'ag "ag" "\
