83f62568b95c9213dcbc7af666a42cf4681a144c
Lincoln Lee <git@lincoln.hk>
2015-12-29 10:29:23 +0800

    Add smart-beginning-of-line

--- init.el
+++ init.el
@@ -38,6 +38,17 @@
                   'kill-this-buffer)
   (global-set-key (kbd "s-1")
                   'delete-other-windows))
+                  
+(defun smart-beginning-of-line ()
+  "Move point to first non-whitespace character or `beginning-of-line'."
+  (interactive)
+  (let ((oldpos (point)))
+    (back-to-indentation)
+    (and (= oldpos (point))
+         (beginning-of-line))))
+
+(global-set-key (kbd "C-a") 'smart-beginning-of-line)
+
 
 (require 'use-package)
 
