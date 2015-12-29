0326ef2209f3b03aae297e0785d4f903d905582d
Lincoln Lee <git@lincoln.hk>
2015-12-29 09:04:29 +0800

    [Updating yasnippet version: 20151227.1550][removing old version: 20151212.2133]

--- elpa/yasnippet-20151227.1550/yasnippet-autoloads.el
+++ elpa/yasnippet-20151227.1550/yasnippet-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "yasnippet" "yasnippet.el" (22125 35295 0 0))
+;;;### (autoloads nil "yasnippet" "yasnippet.el" (22145 13328 0 0))
 ;;; Generated autoloads from yasnippet.el
 
 (autoload 'yas-minor-mode "yasnippet" "\
@@ -45,7 +45,7 @@ See `yas-minor-mode' for more information on Yas minor mode.
 
 ;;;***
 
-;;;### (autoloads nil nil ("yasnippet-pkg.el") (22125 35295 898256
+;;;### (autoloads nil nil ("yasnippet-pkg.el") (22145 13328 570008
 ;;;;;;  0))
 
 ;;;***
