62b0369b85215dd86b204d7ed1d31492a61a8a99
Lincoln Lee <git@lincoln.hk>
2015-12-30 21:27:24 +0800

    [Updating popup-imenu version: 20151229.900][removing old version: 20151219.1709]

--- elpa/popup-imenu-20151229.900/popup-imenu-autoloads.el
+++ elpa/popup-imenu-20151229.900/popup-imenu-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "popup-imenu" "popup-imenu.el" (22146 48307
+;;;### (autoloads nil "popup-imenu" "popup-imenu.el" (22147 56095
 ;;;;;;  0 0))
 ;;; Generated autoloads from popup-imenu.el
 
