6d14c1bdc27cd3ed8c8ba1be0e69cb58fba19277
Lincoln Lee <git@lincoln.hk>
2016-01-19 01:38:28 +0000

    [Updating with-editor version: 20160118.635][removing old version: 20160115.1233]

--- elpa/with-editor-20160118.635/with-editor-autoloads.el
+++ elpa/with-editor-20160118.635/with-editor-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
 ;;;### (autoloads nil nil ("with-editor-pkg.el" "with-editor.el")
-;;;;;;  (22170 28601 404589 0))
+;;;;;;  (22173 37750 143802 444000))
 
 ;;;***
 
