9377594893c9cc2b251425c3cb6f983507de3694
Lincoln Lee <git@lincoln.hk>
2016-01-20 16:46:00 +0000

    [Updating avy version: 20160120.22][removing old version: 20160106.723]

--- elpa/avy-20160120.22/avy-autoloads.el
+++ elpa/avy-20160120.22/avy-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "avy" "avy.el" (22157 44848 0 0))
+;;;### (autoloads nil "avy" "avy.el" (22175 47550 805019 250000))
 ;;; Generated autoloads from avy.el
 
 (autoload 'avy-goto-char "avy" "\
