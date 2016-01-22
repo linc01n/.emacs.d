321e5abe938d3b81dd0d0c9f3a6f8912191f2955
Lincoln Lee <git@lincoln.hk>
2016-01-22 09:16:31 +0000

    [Updating robe version: 20160121.1551][removing old version: 20151230.449]

--- elpa/robe-20160121.1551/robe-autoloads.el
+++ elpa/robe-20160121.1551/robe-autoloads.el
@@ -3,7 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "ac-robe" "ac-robe.el" (22155 56095 0 0))
+;;;### (autoloads nil "ac-robe" "ac-robe.el" (22177 62295 883916
+;;;;;;  104000))
 ;;; Generated autoloads from ac-robe.el
 
 (autoload 'ac-robe-available "ac-robe" "\
@@ -21,8 +22,8 @@ Return t if `robe-mode' completions are available, otherwise nil.
 
 ;;;***
 
-;;;### (autoloads nil "company-robe" "company-robe.el" (22155 56096
-;;;;;;  0 0))
+;;;### (autoloads nil "company-robe" "company-robe.el" (22177 62295
+;;;;;;  895294 194000))
 ;;; Generated autoloads from company-robe.el
 
 (autoload 'company-robe "company-robe" "\
@@ -32,7 +33,7 @@ A `company-mode' completion back-end for `robe-mode'.
 
 ;;;***
 
-;;;### (autoloads nil "robe" "robe.el" (22155 56095 0 0))
+;;;### (autoloads nil "robe" "robe.el" (22177 62295 757850 542000))
 ;;; Generated autoloads from robe.el
 
 (autoload 'robe-mode "robe" "\
@@ -46,7 +47,7 @@ The following commands are available:
 
 ;;;***
 
-;;;### (autoloads nil nil ("robe-pkg.el") (22155 56096 18766 0))
+;;;### (autoloads nil nil ("robe-pkg.el") (22177 62295 917381 600000))
 
 ;;;***
 
