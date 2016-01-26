3d943d638c4d850e41ec13f2ace053a949a58615
Lincoln Lee <git@lincoln.hk>
2016-01-26 06:43:38 +0000

    Add aspell config

--- available-init/flyspell.el
+++ available-init/flyspell.el
@@ -3,4 +3,6 @@
   (setq flyspell-issue-message-flag nil)
   (local-unset-key (kbd "C-;"))
   :config
-  (add-hook 'prog-mode-hook (lambda ()(flyspell-prog-mode))))
+  (add-hook 'prog-mode-hook (lambda ()(flyspell-prog-mode)))
+  (setq ispell-program-name "aspell"
+        ispell-extra-args '("--sug-mode=ultra" "--lang=en_US" "--run-together" "--run-together-limit=5" "--run-together-min=2")))
