a8175997e44e9c069c203016b372f76b8960c893
Lincoln Lee <git@lincoln.hk>
2016-01-27 21:06:58 +0800

    Use bind for evil

--- available-init/evil.el
+++ available-init/evil.el
@@ -1,5 +1,5 @@
 (req-package evil
+  :bind ("M-i" . evil-mode)
   :config
-  (global-set-key (kbd "M-i") #'evil-mode)
   (define-key evil-normal-state-map (kbd "s-i") #'evil-insert)
   (define-key evil-insert-state-map (kbd "s-i") #'evil-normal-state))
