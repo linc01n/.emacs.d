c2bb41e5fd1b5e0300951d94a26db1c430902adf
Lincoln Lee <git@lincoln.hk>
2016-01-27 20:55:28 +0800

    Fix keymap not working in evil mode map

--- available-init/evil.el
+++ available-init/evil.el
@@ -1,5 +1,5 @@
 (req-package evil
-  :bind ("M-i" . evil-mode)
   :config
-  ((bind-key "s-i" 'evil-insert evil-normal-state-map)
-   (bind-key "s-i" 'evil-normal-state evil-insert-state-map)))
+  (global-set-key (kbd "M-i") #'evil-mode)
+  (define-key evil-normal-state-map (kbd "s-i") #'evil-insert)
+  (define-key evil-insert-state-map (kbd "s-i") #'evil-normal-state))
