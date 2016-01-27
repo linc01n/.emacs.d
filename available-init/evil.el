45eff4ac18c017089fdee3d3df89ed66724746cb
Lincoln Lee <git@lincoln.hk>
2016-01-27 14:34:17 +0800

    Add evil state toggle keybinding

--- available-init/evil.el
+++ available-init/evil.el
@@ -1,2 +1,5 @@
 (req-package evil
-  :bind ("M-i" . evil-mode))
+  :bind ("M-i" . evil-mode)
+  :config
+  ((bind-key "s-i" 'evil-insert evil-normal-state-map)
+   (bind-key "s-i" 'evil-normal-state evil-insert-state-map)))
