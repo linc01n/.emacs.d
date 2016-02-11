1482c6dca9f6c0edbd332223f8a8a42db4d0226f
Lincoln Lee <git@lincoln.hk>
2016-02-11 10:35:10 +0800

    [Updating git-commit version: 20160130.649][removing old version: 20160119.1409]

--- elpa/git-commit-20160130.649/git-commit-autoloads.el
+++ elpa/git-commit-20160130.649/git-commit-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "git-commit" "git-commit.el" (22175 47550 202330
-;;;;;;  723000))
+;;;### (autoloads nil "git-commit" "git-commit.el" (22203 62146 0
+;;;;;;  0))
 ;;; Generated autoloads from git-commit.el
 
 (defvar global-git-commit-mode t "\
@@ -24,11 +24,6 @@ when Git uses the Emacsclient as $GIT_EDITOR to have the user
 provide such a commit message.
 
 \(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("git-commit-pkg.el") (22175 47550 214615
-;;;;;;  460000))
 
 ;;;***
 
