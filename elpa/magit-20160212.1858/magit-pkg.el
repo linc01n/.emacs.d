8211da67c0d500a58e2ccb377551aea32384040f
Lincoln Lee <git@lincoln.hk>
2016-02-14 15:14:03 +0800

    [Updating magit version: 20160212.1858][removing old version: 20160211.2105]

--- elpa/magit-20160212.1858/magit-pkg.el
+++ elpa/magit-20160212.1858/magit-pkg.el
@@ -1,4 +1,4 @@
-(define-package "magit" "20160211.2105" "A Git porcelain inside Emacs"
+(define-package "magit" "20160212.1858" "A Git porcelain inside Emacs"
   '((emacs "24.4")
     (async "20150909.2257")
     (dash "20151021.113")
