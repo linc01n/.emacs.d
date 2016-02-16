ac40ff6a56b7013a4a86c873a1a6104ff59666ba
Lincoln Lee <git@lincoln.hk>
2016-02-16 22:20:04 +0800

    [Updating swiper version: 20160215.140][removing old version: 20160212.122]

--- elpa/swiper-20160215.140/swiper-autoloads.el
+++ elpa/swiper-20160215.140/swiper-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "ivy" "ivy.el" (22206 39195 0 0))
+;;;### (autoloads nil "ivy" "ivy.el" (22211 12208 0 0))
 ;;; Generated autoloads from ivy.el
 
 (autoload 'ivy-resume "ivy" "\
@@ -17,11 +17,11 @@ Read a string in the minibuffer, with completion.
 This interface conforms to `completing-read' and can be used for
 `completing-read-function'.
 
-PROMPT is a string to prompt with; normally it ends in a colon and a space.
-COLLECTION can be a list of strings, an alist, an obarray or a hash table.
+PROMPT is a string that normally ends in a colon and a space.
+COLLECTION is either a list of strings, an alist, an obarray, or a hash table.
 PREDICATE limits completion to a subset of COLLECTION.
-REQUIRE-MATCH is specified with a boolean value.  See `completing-read'.
-INITIAL-INPUT is a string that can be inserted into the minibuffer initially.
+REQUIRE-MATCH is a boolean value.  See `completing-read'.
+INITIAL-INPUT is a string inserted into the minibuffer initially.
 HISTORY is a list of previously selected inputs.
 DEF is the default value.
 INHERIT-INPUT-METHOD is currently ignored.
@@ -68,7 +68,7 @@ Find a file on `recentf-list'.
 
 ;;;***
 
-;;;### (autoloads nil "swiper" "swiper.el" (22206 39195 0 0))
+;;;### (autoloads nil "swiper" "swiper.el" (22211 12208 0 0))
 ;;; Generated autoloads from swiper.el
 
 (autoload 'swiper-avy "swiper" "\
@@ -90,7 +90,7 @@ When non-nil, INITIAL-INPUT is the initial search pattern.
 ;;;***
 
 ;;;### (autoloads nil nil ("colir.el" "ivy-hydra.el" "swiper-pkg.el")
-;;;;;;  (22206 39195 607212 0))
+;;;;;;  (22211 12208 887193 0))
 
 ;;;***
 
