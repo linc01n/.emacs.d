73d97f4f3370defe2564c8b6574b266e3795b883
Lincoln Lee <git@lincoln.hk>
2016-02-23 15:39:51 +0000

    Add org archive

--- init.el
+++ init.el
@@ -1,6 +1,7 @@
 (require 'package)
 (setq package-archives '(("gnu" ."http://elpa.gnu.org/packages/")
-                         ("melpa" ."http://melpa.org/packages/")))
+                         ("melpa" ."http://melpa.org/packages/")
+                         ("org". "http://orgmode.org/elpa/")))
 
 
 (package-initialize)
