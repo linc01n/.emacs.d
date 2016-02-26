20887c87f9b23fb2ffd019c0b3eea6f996e67e25
Lincoln Lee <git@lincoln.hk>
2016-02-26 15:37:42 +0800

    Remove flyspell key binding to iedit

--- available-init/flyspell.el
+++ available-init/flyspell.el
@@ -1,8 +1,8 @@
 (req-package flyspell
   :init
   (setq flyspell-issue-message-flag nil)
-  (local-unset-key (kbd "C-;"))
   :config
+  (define-key flyspell-mode-map (kbd "C-;") nil)
   (add-hook 'prog-mode-hook (lambda ()(flyspell-prog-mode)))
   (setq ispell-program-name "aspell"
         ispell-extra-args '("--sug-mode=ultra" "--lang=en_US" "--run-together" "--run-together-limit=5" "--run-together-min=2")))
