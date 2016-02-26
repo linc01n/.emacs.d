f8c64b8f1295f8f59980bf9464595baebf7b9683
Lincoln Lee <git@lincoln.hk>
2016-02-26 14:08:47 +0800

    Disable ido tab completion

--- available-init/ido.el
+++ available-init/ido.el
@@ -5,6 +5,8 @@
 
 (req-package ido
   :require ido-vertical-mode flx-ido
+  :init
+  (defun ido-tab-do-nothing () (interactive))
   :config
   (setq ido-create-new-buffer 'always)
   (setq ido-enable-flex-matching t)
@@ -12,7 +14,11 @@
   (ido-mode 1)
   (ido-vertical-mode 1)
   (setq ido-vertical-define-keys 'C-n-C-p-up-down-left-right)
-  (flx-ido-mode 1))
+  (flx-ido-mode 1)
+  (bind-keys :map ido-completion-map        ("TAB" . ido-tab-do-nothing))
+  (bind-keys :map ido-common-completion-map ("TAB" . ido-tab-do-nothing))
+  (bind-keys :map ido-file-completion-map   ("TAB" . ido-tab-do-nothing))
+  (bind-keys :map ido-buffer-completion-map ("TAB" . ido-tab-do-nothing)))
 
 (req-package ido-vertical-mode)
 
