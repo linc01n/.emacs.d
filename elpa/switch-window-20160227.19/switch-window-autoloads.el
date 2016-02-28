a0bba6748fd69d604e48016f100d118ffbb63ef1
Lincoln Lee <git@lincoln.hk>
2016-02-28 20:31:45 +0800

    [Updating switch-window version: 20160227.19][removing old version: 20160224.812]

--- elpa/switch-window-20160227.19/switch-window-autoloads.el
+++ elpa/switch-window-20160227.19/switch-window-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "switch-window" "switch-window.el" (22223 60072
+;;;### (autoloads nil "switch-window" "switch-window.el" (22226 59418
 ;;;;;;  0 0))
 ;;; Generated autoloads from switch-window.el
 
@@ -24,6 +24,31 @@ Display an overlay in each window showing a unique key, then
 ask user for the window where move to
 
 \(fn)" t nil)
+
+(autoload 'switch-window-then-split-horizontally "switch-window" "\
+Select a window then split it horizontally.
+
+\(fn ARG)" t nil)
+
+(autoload 'switch-window-then-split-vertically "switch-window" "\
+Select a window then split it vertically.
+
+\(fn ARG)" t nil)
+
+(autoload 'switch-window-then-split-below "switch-window" "\
+Select a window then split it with split-window-below's mode.
+
+\(fn ARG)" t nil)
+
+(autoload 'switch-window-then-split-right "switch-window" "\
+Select a window then split it with split-window-right's mode.
+
+\(fn ARG)" t nil)
+
+(autoload 'switch-window-then-swap-buffer "switch-window" "\
+Select a window then swap it buffer with current window's buffer.
+
+\(fn ARG)" t nil)
 
 ;;;***
 
