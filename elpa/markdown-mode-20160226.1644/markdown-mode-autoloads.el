e321dbd08571ac2a7077a3392b7d1b12062f5062
Lincoln Lee <git@lincoln.hk>
2016-03-02 23:38:26 +0800

    [Updating markdown-mode version: 20160226.1644][removing old version: 20160226.918]

--- elpa/markdown-mode-20160226.1644/markdown-mode-autoloads.el
+++ elpa/markdown-mode-20160226.1644/markdown-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "markdown-mode" "markdown-mode.el" (22226 59435
+;;;### (autoloads nil "markdown-mode" "markdown-mode.el" (22231 2274
 ;;;;;;  0 0))
 ;;; Generated autoloads from markdown-mode.el
 
