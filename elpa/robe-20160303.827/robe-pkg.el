e0229a842ee67a63decf1d800a5bfaef90f85d61
Lincoln Lee <git@lincoln.hk>
2016-03-04 22:29:45 +0800

    [Updating robe version: 20160303.827][removing old version: 20160229.439]

--- elpa/robe-20160303.827/robe-pkg.el
+++ elpa/robe-20160303.827/robe-pkg.el
@@ -1,4 +1,4 @@
-(define-package "robe" "20160229.439" "Code navigation, documentation lookup and completion for Ruby"
+(define-package "robe" "20160303.827" "Code navigation, documentation lookup and completion for Ruby"
   '((inf-ruby "2.3.0"))
   :url "https://github.com/dgutov/robe" :keywords
   '("ruby" "convenience" "rails"))
