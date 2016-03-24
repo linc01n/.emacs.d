264c3e6b47e392c1a195b5f7143b21644e0c7608
Lincoln Lee <git@lincoln.hk>
2016-03-24 22:25:07 +0800

    [Updating evil version: 20160324.525][removing old version: 20160321.1722]

--- elpa/evil-20160324.525/evil-pkg.el
+++ elpa/evil-20160324.525/evil-pkg.el
@@ -1,4 +1,4 @@
-(define-package "evil" "20160321.1722" "Extensible Vi layer for Emacs."
+(define-package "evil" "20160324.525" "Extensible Vi layer for Emacs."
   '((undo-tree "0.6.3")
     (goto-chg "1.6")))
 ;; Local Variables:
