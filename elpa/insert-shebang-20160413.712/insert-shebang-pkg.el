4179aea7b1d40c90f796290a1adc9df554390ab4
Lincoln Lee <git@lincoln.hk>
2016-05-05 01:17:37 +0800

    [Removing insert-shebang version: 20160413.712]

--- elpa/insert-shebang-20160413.712/insert-shebang-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "insert-shebang" "20160413.712" "Insert shebang line automatically." 'nil :url "http://github.com/psachin/insert-shebang" :keywords '("shebang" "tool" "convenience"))
