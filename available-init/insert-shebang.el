bf67678a1e95a347c35b4c2bb794dea0b2127851
Lincoln Lee <git@lincoln.hk>
2016-05-05 10:44:07 +0800

    Remove insert-shebang on find-file-hook

--- available-init/insert-shebang.el
+++ available-init/insert-shebang.el
@@ -1,10 +1,12 @@
 (req-package insert-shebang
   :init
   (setq insert-shebang-file-types
-         '(("py" . "python")
-           ("sh" . "bash")
-           ("pl" . "perl")
-           ("rb" . "ruby")))
+        '(("py" . "python")
+          ("sh" . "bash")
+          ("pl" . "perl")
+          ("rb" . "ruby")))
   (setq insert-shebang-track-ignored-filename nil)
   :config
-  (add-to-list 'find-file-not-found-functions 'insert-shebang))
+  (progn
+    (remove-hook 'find-file-hook 'insert-shebang)
+    (add-to-list 'find-file-not-found-functions 'insert-shebang)))
