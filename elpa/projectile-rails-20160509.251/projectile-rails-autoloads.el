06a5687c9df4989efec0ebc65fa8cfa2f546d925
Lincoln Lee <git@lincoln.hk>
2016-05-10 12:33:05 +0800

    [Updating projectile-rails version: 20160509.251][removing old version: 20160417.506]

--- elpa/projectile-rails-20160509.251/projectile-rails-autoloads.el
+++ elpa/projectile-rails-20160509.251/projectile-rails-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "projectile-rails" "projectile-rails.el" (22316
-;;;;;;  26503 0 0))
+;;;### (autoloads nil "projectile-rails" "projectile-rails.el" (22321
+;;;;;;  25697 0 0))
 ;;; Generated autoloads from projectile-rails.el
 
 (autoload 'projectile-rails-mode "projectile-rails" "\
