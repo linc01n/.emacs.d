41a5e0201e6822203036ee01d74afc9359ad18bc
Lincoln Lee <git@lincoln.hk>
2016-05-11 11:00:57 +0800

    Remove custom set variables

--- init.el
+++ init.el
@@ -22,20 +22,3 @@
   (load-dirs-reload))
 
 (req-package-finish)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(blink-cursor-mode nil)
- '(current-language-environment "UTF-8")
- '(package-selected-packages
-   (quote
-    (literate-coffee-mode yasnippet yaml-mode whitespace-cleanup-mode which-key vlf visual-regexp-steroids tabbar switch-window swiper string-edit spaceline solarized-theme smooth-scrolling smex smartparens rvm robe req-package rainbow-delimiters projectile-rails occur-x nlinum neotree multiple-cursors move-text markdown-mode magit load-dir jsx-mode json-mode js2-mode insert-shebang indent-guide iedit ido-vertical-mode ido-ubiquitous hc-zenburn-theme haml-mode git-timemachine git-gutter-fringe flx-ido expand-region exec-path-from-shell evil edit-server discover-my-major dired+ darktooth-theme company coffee-mode avy anzu ag)))
- '(tab-width 2))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
