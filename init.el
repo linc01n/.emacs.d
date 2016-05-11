846b061c1b02f793ffd73ea1ff4c612b4a46c5fd
Lincoln Lee <git@lincoln.hk>
2016-05-11 10:30:02 +0800

    Use non https for orgmode

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 (require 'package)
 (setq package-archives '(("gnu" ."https://elpa.gnu.org/packages/")
                          ("melpa" ."https://melpa.org/packages/")
-                         ("org". "https://orgmode.org/elpa/")))
+                         ("org". "http://orgmode.org/elpa/")))
 
 
 (package-initialize)
