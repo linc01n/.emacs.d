33a2e3b87b5ea7a9034fc4e6e90161d8f5d89d83
Lincoln Lee <git@lincoln.hk>
2016-05-12 14:59:32 +0800

    Add minted for latex code block

--- available-init/org-mode.el
+++ available-init/org-mode.el
@@ -1,5 +1,15 @@
 (req-package org
-  :mode ("\\.org\\'" . org-mode))
+  :mode ("\\.org\\'" . org-mode)
+  :config
+  (progn
+    (add-to-list 'org-latex-packages-alist '("" "minted"))
+    (setq org-latex-listings 'minted)
+    (setq org-latex-pdf-process
+          '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+            "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+            "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
+    (setq org-latex-minted-options '(("breaklines" "true")))))
+
 
 (req-package org-bullets
   :require org
