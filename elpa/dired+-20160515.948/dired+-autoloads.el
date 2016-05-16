51fe5e49e428136f1eb1dd2479387fcd391f3045
Lincoln Lee <git@lincoln.hk>
2016-05-16 17:48:56 +0800

    [Updating dired+ version: 20160515.948][removing old version: 20160510.1649]

--- elpa/dired+-20160515.948/dired+-autoloads.el
+++ elpa/dired+-20160515.948/dired+-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "dired+" "dired+.el" (22324 43113 0 0))
+;;;### (autoloads nil "dired+" "dired+.el" (22329 38774 0 0))
 ;;; Generated autoloads from dired+.el
 
 (defvar diff-switches "-c" "\
