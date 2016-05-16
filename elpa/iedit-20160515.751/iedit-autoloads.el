09cbd67c51ac585b56605c2f727c297dfa19f9fc
Lincoln Lee <git@lincoln.hk>
2016-05-16 17:48:56 +0800

    [Updating iedit version: 20160515.751][removing old version: 20160514.739]

--- elpa/iedit-20160515.751/iedit-autoloads.el
+++ elpa/iedit-20160515.751/iedit-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "iedit" "iedit.el" (22328 2788 0 0))
+;;;### (autoloads nil "iedit" "iedit.el" (22329 38772 0 0))
 ;;; Generated autoloads from iedit.el
 
 (autoload 'iedit-mode "iedit" "\
@@ -68,7 +68,7 @@ Toggle Iedit mode on current function.
 
 ;;;***
 
-;;;### (autoloads nil "iedit-rect" "iedit-rect.el" (22328 2788 0
+;;;### (autoloads nil "iedit-rect" "iedit-rect.el" (22329 38772 0
 ;;;;;;  0))
 ;;; Generated autoloads from iedit-rect.el
 
@@ -86,8 +86,8 @@ Commands:
 
 ;;;***
 
-;;;### (autoloads nil nil ("iedit-lib.el" "iedit-pkg.el") (22328
-;;;;;;  2788 768895 0))
+;;;### (autoloads nil nil ("iedit-lib.el" "iedit-pkg.el") (22329
+;;;;;;  38772 353967 0))
 
 ;;;***
 
