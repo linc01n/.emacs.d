c699967deae0e7064f99aaee9e921d9de0692fbc
Lincoln Lee <git@lincoln.hk>
2016-05-17 23:48:29 +0800

    [Updating which-key version: 20160516.1806][removing old version: 20160513.605]

--- elpa/which-key-20160516.1806/which-key-autoloads.el
+++ elpa/which-key-20160516.1806/which-key-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "which-key" "which-key.el" (22326 38973 0 0))
+;;;### (autoloads nil "which-key" "which-key.el" (22331 15673 0 0))
 ;;; Generated autoloads from which-key.el
 
 (defvar which-key-mode nil "\
