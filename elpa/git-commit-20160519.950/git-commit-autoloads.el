cda616cabcea322124d999aa17fce4c408c2314e
Lincoln Lee <git@lincoln.hk>
2016-05-20 08:33:03 +0800

    [Updating git-commit version: 20160519.950][removing old version: 20160425.430]

--- elpa/git-commit-20160519.950/git-commit-autoloads.el
+++ elpa/git-commit-20160519.950/git-commit-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "git-commit" "git-commit.el" (22322 39080 0
+;;;### (autoloads nil "git-commit" "git-commit.el" (22334 23303 0
 ;;;;;;  0))
 ;;; Generated autoloads from git-commit.el
 
