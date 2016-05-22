1147654ddfca6c84ad0f4c86e4ea54583002a1cc
Lincoln Lee <git@lincoln.hk>
2016-05-22 17:00:23 +0800

    [Updating coffee-mode version: 20160520.146][removing old version: 20160419.1947]

--- elpa/coffee-mode-20160520.146/coffee-mode-autoloads.el
+++ elpa/coffee-mode-20160520.146/coffee-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "coffee-mode" "coffee-mode.el" (22322 39043
+;;;### (autoloads nil "coffee-mode" "coffee-mode.el" (22337 29844
 ;;;;;;  0 0))
 ;;; Generated autoloads from coffee-mode.el
 
