495f4ffee41dd319f08b8466af21421d72fee89d
Lincoln Lee <git@lincoln.hk>
2016-05-22 17:00:24 +0800

    [Updating git-gutter version: 20160520.1655][removing old version: 20160409.713]

--- elpa/git-gutter-20160520.1655/git-gutter-autoloads.el
+++ elpa/git-gutter-20160520.1655/git-gutter-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "git-gutter" "git-gutter.el" (22322 39060 0
+;;;### (autoloads nil "git-gutter" "git-gutter.el" (22337 29843 0
 ;;;;;;  0))
 ;;; Generated autoloads from git-gutter.el
 
