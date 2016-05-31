2f9f2ebef7efee288bd5fa25aa39c065a2c853fd
Lincoln Lee <git@lincoln.hk>
2016-05-31 20:43:58 +0800

    [Updating magit-popup version: 20160529.2045][removing old version: 20160521.1221]

--- elpa/magit-popup-20160529.2045/magit-popup-autoloads.el
+++ elpa/magit-popup-20160529.2045/magit-popup-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
 ;;;### (autoloads nil nil ("magit-popup-pkg.el" "magit-popup.el")
-;;;;;;  (22337 29826 879033 0))
+;;;;;;  (22349 33390 431432 0))
 
 ;;;***
 
