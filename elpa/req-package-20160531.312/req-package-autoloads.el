6d8f6b9550ba63e6043238f8f90e888bb92a330e
Lincoln Lee <git@lincoln.hk>
2016-06-01 20:45:25 +0800

    [Updating req-package version: 20160531.312][removing old version: 20160529.1913]

--- elpa/req-package-20160531.312/req-package-autoloads.el
+++ elpa/req-package-20160531.312/req-package-autoloads.el
@@ -5,7 +5,7 @@
 
 ;;;### (autoloads nil nil ("req-package-args.el" "req-package-cycles.el"
 ;;;;;;  "req-package-hooks.el" "req-package-pkg.el" "req-package-providers.el"
-;;;;;;  "req-package.el") (22348 2598 954178 0))
+;;;;;;  "req-package.el") (22350 55411 734560 0))
 
 ;;;***
 
