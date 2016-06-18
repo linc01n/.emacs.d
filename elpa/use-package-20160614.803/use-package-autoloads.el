a0f6fd0777b5d231e405d05d976c88ef31e4ea04
Lincoln Lee <git@lincoln.hk>
2016-06-18 11:53:39 +0800

    [Updating use-package version: 20160614.803][removing old version: 20160613.957]

--- elpa/use-package-20160614.803/use-package-autoloads.el
+++ elpa/use-package-20160614.803/use-package-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "use-package" "use-package.el" (22367 25235
+;;;### (autoloads nil "use-package" "use-package.el" (22372 50407
 ;;;;;;  0 0))
 ;;; Generated autoloads from use-package.el
 
--- elpa/use-package-20160614.803/use-package.el
+++ elpa/use-package-20160614.803/use-package.el
@@ -7,7 +7,7 @@
 ;; Created: 17 Jun 2012
 ;; Modified: 26 Sep 2015
 ;; Version: 2.1
-;; Package-Version: 20160613.957
+;; Package-Version: 20160614.803
 ;; Package-Requires: ((bind-key "1.0") (diminish "0.44"))
 ;; Keywords: dotemacs startup speed config package
 ;; URL: https://github.com/jwiegley/use-package
@@ -178,6 +178,9 @@ Must be set before loading use-package."
   :group 'use-package)
 
 (when use-package-enable-imenu-support
+  ;; Not defined in Emacs 24
+  (defvar lisp-mode-symbol-regexp
+    "\\(?:\\sw\\|\\s_\\|\\\\.\\)+")
   (add-to-list
    'lisp-imenu-generic-expression
    (list "Package"
