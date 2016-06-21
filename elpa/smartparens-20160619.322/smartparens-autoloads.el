610ca9c3ce5d26862fc530648b1b30304873e9ed
Lincoln Lee <git@lincoln.hk>
2016-06-21 15:38:45 +0800

    [Updating smartparens version: 20160619.322][removing old version: 20160614.1248]

--- elpa/smartparens-20160619.322/smartparens-autoloads.el
+++ elpa/smartparens-20160619.322/smartparens-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "smartparens" "smartparens.el" (22372 50414
+;;;### (autoloads nil "smartparens" "smartparens.el" (22376 59309
 ;;;;;;  0 0))
 ;;; Generated autoloads from smartparens.el
 
@@ -182,7 +182,7 @@ Turn off `show-smartparens-mode'.
 ;;;;;;  "smartparens-html.el" "smartparens-latex.el" "smartparens-lua.el"
 ;;;;;;  "smartparens-pkg.el" "smartparens-python.el" "smartparens-racket.el"
 ;;;;;;  "smartparens-ruby.el" "smartparens-rust.el" "smartparens-scala.el")
-;;;;;;  (22372 50414 922763 0))
+;;;;;;  (22376 59309 213344 0))
 
 ;;;***
 
