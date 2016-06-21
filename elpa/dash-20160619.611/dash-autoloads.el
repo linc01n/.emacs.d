195a87dbd1ab937bb941548ff6f94aec343f8328
Lincoln Lee <git@lincoln.hk>
2016-06-21 15:38:44 +0800

    [Updating dash version: 20160619.611][removing old version: 20160605.248]

--- elpa/dash-20160619.611/dash-autoloads.el
+++ elpa/dash-20160619.611/dash-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil nil ("dash.el") (22356 11400 15745 0))
+;;;### (autoloads nil nil ("dash.el") (22376 59359 227731 0))
 
 ;;;***
 
