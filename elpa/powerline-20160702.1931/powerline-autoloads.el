d19f30fb26e0cf2256105432953477331ec1516e
Lincoln Lee <git@lincoln.hk>
2016-07-04 12:27:22 +0800

    [Updating powerline version: 20160702.1931][removing old version: 20160601.724]

--- elpa/powerline-20160702.1931/powerline-autoloads.el
+++ elpa/powerline-20160702.1931/powerline-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "powerline" "powerline.el" (22351 58896 0 0))
+;;;### (autoloads nil "powerline" "powerline.el" (22393 58769 0 0))
 ;;; Generated autoloads from powerline.el
 
 (autoload 'powerline-hud "powerline" "\
@@ -46,8 +46,8 @@ Return empty space using FACE and leaving RESERVE space on the right.
 
 ;;;***
 
-;;;### (autoloads nil "powerline-themes" "powerline-themes.el" (22351
-;;;;;;  58896 0 0))
+;;;### (autoloads nil "powerline-themes" "powerline-themes.el" (22393
+;;;;;;  58769 0 0))
 ;;; Generated autoloads from powerline-themes.el
 
 (autoload 'powerline-default-theme "powerline-themes" "\
@@ -73,7 +73,7 @@ Setup a nano-like mode-line.
 ;;;***
 
 ;;;### (autoloads nil nil ("powerline-pkg.el" "powerline-separators.el")
-;;;;;;  (22351 58896 54934 0))
+;;;;;;  (22393 58769 679024 0))
 
 ;;;***
 
