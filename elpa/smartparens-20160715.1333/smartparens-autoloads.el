4e7ad7696570b1d5f2741f367209829fb87bf7f4
Lincoln Lee <git@lincoln.hk>
2016-07-16 13:47:49 +0800

    [Updating smartparens version: 20160715.1333][removing old version: 20160619.322]

--- elpa/smartparens-20160715.1333/smartparens-autoloads.el
+++ elpa/smartparens-20160715.1333/smartparens-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "smartparens" "smartparens.el" (22376 59309
+;;;### (autoloads nil "smartparens" "smartparens.el" (22409 49674
 ;;;;;;  0 0))
 ;;; Generated autoloads from smartparens.el
 
@@ -178,11 +178,11 @@ Turn off `show-smartparens-mode'.
 
 ;;;***
 
-;;;### (autoloads nil nil ("smartparens-config.el" "smartparens-haskell.el"
-;;;;;;  "smartparens-html.el" "smartparens-latex.el" "smartparens-lua.el"
-;;;;;;  "smartparens-pkg.el" "smartparens-python.el" "smartparens-racket.el"
-;;;;;;  "smartparens-ruby.el" "smartparens-rust.el" "smartparens-scala.el")
-;;;;;;  (22376 59309 213344 0))
+;;;### (autoloads nil nil ("smartparens-clojure.el" "smartparens-config.el"
+;;;;;;  "smartparens-haskell.el" "smartparens-html.el" "smartparens-latex.el"
+;;;;;;  "smartparens-lua.el" "smartparens-pkg.el" "smartparens-python.el"
+;;;;;;  "smartparens-racket.el" "smartparens-ruby.el" "smartparens-rust.el"
+;;;;;;  "smartparens-scala.el") (22409 49674 204121 0))
 
 ;;;***
 
