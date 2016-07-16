9382e02bf920f9efeac474f076db1405cbdc328c
Lincoln Lee <git@lincoln.hk>
2016-07-16 13:47:49 +0800

    [Updating popup version: 20160709.729][removing old version: 20160531.425]

--- elpa/popup-20160709.729/popup-autoloads.el
+++ elpa/popup-20160709.729/popup-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil nil ("popup.el") (22350 55414 933177 0))
+;;;### (autoloads nil nil ("popup.el") (22409 49679 912007 0))
 
 ;;;***
 
