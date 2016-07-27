4160901b22f42e8f04b87feb682aef483fcfe248
Lincoln Lee <git@lincoln.hk>
2016-07-27 18:13:26 +0800

    [Updating zenburn-theme version: 20160724.2141][removing old version: 20160501.749]

--- elpa/zenburn-theme-20160724.2141/zenburn-theme-autoloads.el
+++ elpa/zenburn-theme-20160724.2141/zenburn-theme-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "zenburn-theme" "zenburn-theme.el" (22322 39064
+;;;### (autoloads nil "zenburn-theme" "zenburn-theme.el" (22424 28974
 ;;;;;;  0 0))
 ;;; Generated autoloads from zenburn-theme.el
 
