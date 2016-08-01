e1e2978a0e2b06d3d51f4a7b2471b0e346c7b518
Lincoln Lee <git@lincoln.hk>
2016-08-01 12:56:53 +0800

    [Updating projectile version: 20160728.52][removing old version: 20160723.622]

--- elpa/projectile-20160728.52/projectile-autoloads.el
+++ elpa/projectile-20160728.52/projectile-autoloads.el
@@ -1,9 +1,9 @@
 ;;; projectile-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "projectile" "projectile.el" (22421 32610 0
+;;;### (autoloads nil "projectile" "projectile.el" (22430 54892 0
 ;;;;;;  0))
 ;;; Generated autoloads from projectile.el
 
@@ -425,7 +425,8 @@ Otherwise behave as if called interactively.
 
 (defvar projectile-global-mode nil "\
 Non-nil if Projectile-Global mode is enabled.
-See the command `projectile-global-mode' for a description of this minor mode.
+See the `projectile-global-mode' command
+for a description of this minor mode.
 Setting this variable directly does not take effect;
 either customize it (see the info node `Easy Customization')
 or call the function `projectile-global-mode'.")
