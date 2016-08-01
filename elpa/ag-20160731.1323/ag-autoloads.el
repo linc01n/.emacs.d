662f491522b1a9b164f72e6f490db1e8fb24312d
Lincoln Lee <git@lincoln.hk>
2016-08-01 12:56:51 +0800

    [Updating ag version: 20160731.1323][removing old version: 20160702.1128]

--- elpa/ag-20160731.1323/ag-autoloads.el
+++ elpa/ag-20160731.1323/ag-autoloads.el
@@ -1,9 +1,9 @@
 ;;; ag-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "ag" "ag.el" (22393 58781 0 0))
+;;;### (autoloads nil "ag" "ag.el" (22430 54904 0 0))
 ;;; Generated autoloads from ag.el
 
 (autoload 'ag "ag" "\
