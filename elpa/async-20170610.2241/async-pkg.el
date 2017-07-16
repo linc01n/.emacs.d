c3692257c3d65bbdd57e6fb17e582ad5e1102d33
Lincoln Lee <git@lincoln.hk>
2017-07-16 19:44:33 +0800

    [Updating async version: 20170610.2241][removing old version: 20160711.2235]

--- elpa/async-20170610.2241/async-pkg.el
+++ elpa/async-20170610.2241/async-pkg.el
@@ -1,4 +1,4 @@
-(define-package "async" "20160711.2235" "Asynchronous processing in Emacs" 'nil :keywords
+(define-package "async" "20170610.2241" "Asynchronous processing in Emacs" 'nil :keywords
   '("async")
   :url "https://github.com/jwiegley/emacs-async")
 ;; Local Variables:
