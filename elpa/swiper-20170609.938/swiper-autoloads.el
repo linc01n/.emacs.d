d9f316b0a6a96379191c41a886ef46efcc6f95ff
Lincoln Lee <git@lincoln.hk>
2017-07-16 19:44:40 +0800

    [Updating swiper version: 20170609.938][removing old version: 20160802.240]

--- elpa/swiper-20170609.938/swiper-autoloads.el
+++ elpa/swiper-20170609.938/swiper-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "swiper" "swiper.el" (22437 63756 0 0))
+;;;### (autoloads nil "swiper" "swiper.el" (22887 32333 0 0))
 ;;; Generated autoloads from swiper.el
 
 (autoload 'swiper-avy "swiper" "\
@@ -16,6 +16,11 @@ Jump to one of the current swiper candidates.
 When non-nil, INITIAL-INPUT is the initial search pattern.
 
 \(fn &optional INITIAL-INPUT)" t nil)
+
+(autoload 'swiper-all "swiper" "\
+Run `swiper' for all open buffers.
+
+\(fn)" t nil)
 
 ;;;***
 
