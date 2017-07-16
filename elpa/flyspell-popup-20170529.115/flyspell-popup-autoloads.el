53921b777e023f144aa1242d0e94f4a63cdc5020
Lincoln Lee <git@lincoln.hk>
2017-07-16 19:44:35 +0800

    [Updating flyspell-popup version: 20170529.115][removing old version: 20150926.803]

--- elpa/flyspell-popup-20170529.115/flyspell-popup-autoloads.el
+++ elpa/flyspell-popup-20170529.115/flyspell-popup-autoloads.el
@@ -1,10 +1,10 @@
 ;;; flyspell-popup-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "flyspell-popup" "flyspell-popup.el" (22322
-;;;;;;  39059 0 0))
+;;;### (autoloads nil "flyspell-popup" "flyspell-popup.el" (22887
+;;;;;;  32403 0 0))
 ;;; Generated autoloads from flyspell-popup.el
 
 (autoload 'flyspell-popup-correct "flyspell-popup" "\
--- elpa/flyspell-popup-20170529.115/flyspell-popup.el
+++ elpa/flyspell-popup-20170529.115/flyspell-popup.el
@@ -1,12 +1,12 @@
 ;;; flyspell-popup.el --- Correcting words with Flyspell in popup menus
 
-;; Copyright (C) 2015  Chunyang Xu
+;; Copyright (C) 2015, 2017  Chunyang Xu
 
-;; Author: Chunyang Xu <xuchunyang56@gmail.com>
+;; Author: Chunyang Xu <mail@xuchunyang.me>
 ;; Keywords: convenience
-;; Package-Version: 20150926.803
+;; Package-Version: 20170529.115
 ;; URL: https://github.com/xuchunyang/flyspell-popup
-;; Version: 0.2
+;; Version: 0.3
 ;; Package-Requires: ((popup "0.5.0"))
 ;; Created: Sun Jun 28 15:23:05 CST 2015
 
@@ -95,10 +95,13 @@ Adapted from `flyspell-correct-word-before-point'."
                                          :value (cons 'session word))
                         (popup-make-item (format "Accept (buffer) \"%s\"" word)
                                          :value (cons 'buffer word))))
-                      :margin t)))
+                      :margin t
+                      :fallback (lambda (_event _default)
+                                  (keyboard-quit)))))
                 (cond ((stringp res)
                        (flyspell-do-correct
-                        res poss word cursor-location start end opoint))
+                        (substring-no-properties res)
+                        poss word cursor-location start end opoint))
                       (t
                        (let ((cmd (car res))
                              (wrd (cdr res)))
