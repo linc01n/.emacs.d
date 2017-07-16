459b6336bb193b7cd320738dae89656d8485d822
Lincoln Lee <git@lincoln.hk>
2017-07-16 19:45:32 +0800

    Load GOPATH from shell

--- available-init/exec-path-from-shell.el
+++ available-init/exec-path-from-shell.el
@@ -2,4 +2,5 @@
   :config
   (progn
     (when (memq window-system '(mac ns))
-      (exec-path-from-shell-initialize))))
+      (exec-path-from-shell-initialize)
+      (exec-path-from-shell-copy-env "GOPATH"))))
