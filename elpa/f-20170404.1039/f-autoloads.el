f7a7b3cd1153204097899fa3e37b77c257070d1c
Lincoln Lee <git@lincoln.hk>
2017-07-16 19:44:34 +0800

    [Updating f version: 20170404.1039][removing old version: 20160426.527]

--- elpa/f-20170404.1039/f-autoloads.el
+++ elpa/f-20170404.1039/f-autoloads.el
@@ -1,9 +1,9 @@
 ;;; f-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil nil ("f.el") (22326 39137 970532 0))
+;;;### (autoloads nil nil ("f.el") (22887 32409 0 0))
 
 ;;;***
 
