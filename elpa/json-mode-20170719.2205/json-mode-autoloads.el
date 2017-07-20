c74f294381fff82128a9404333817724aa41cff4
Lincoln Lee <git@lincoln.hk>
2017-07-20 23:36:31 +0800

    [Updating json-mode version: 20170719.2205][removing old version: 20170619.1701]

--- elpa/json-mode-20170719.2205/json-mode-autoloads.el
+++ elpa/json-mode-20170719.2205/json-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "json-mode" "json-mode.el" (22887 32378 0 0))
+;;;### (autoloads nil "json-mode" "json-mode.el" (22896 52726 0 0))
 ;;; Generated autoloads from json-mode.el
 
 (defconst json-mode-standard-file-ext '(".json" ".jsonld") "\
@@ -33,6 +33,11 @@ Major mode for editing JSON files
 \(fn)" t nil)
 
 (autoload 'json-mode-show-path "json-mode" "\
+
+
+\(fn)" t nil)
+
+(autoload 'json-mode-kill-path "json-mode" "\
 
 
 \(fn)" t nil)
