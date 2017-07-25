33b11ecbd13dc30e4af24b2aa9cfc876c90bb291
Lincoln Lee <git@lincoln.hk>
2017-07-26 00:49:05 +0800

    [Updating projectile version: 20170724.2243][removing old versions: 20170416.148,20170722.357]

--- elpa/projectile-20170724.2243/projectile-autoloads.el
+++ elpa/projectile-20170724.2243/projectile-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "projectile" "projectile.el" (22900 21684 0
+;;;### (autoloads nil "projectile" "projectile.el" (22903 29915 0
 ;;;;;;  0))
 ;;; Generated autoloads from projectile.el
 
