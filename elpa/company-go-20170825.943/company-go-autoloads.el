8b7358422f2cdded0bf02a455df32512bff8b828
Lincoln Lee <git@lincoln.hk>
2017-09-19 06:59:38 +0800

    [Updating company-go version: 20170825.943][removing old version: 20170420.515]

--- elpa/company-go-20170825.943/company-go-autoloads.el
+++ elpa/company-go-20170825.943/company-go-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "company-go" "company-go.el" (22887 32426 0
+;;;### (autoloads nil "company-go" "company-go.el" (22976 20413 0
 ;;;;;;  0))
 ;;; Generated autoloads from company-go.el
 
