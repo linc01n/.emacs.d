36129b9f36b3766ed411a8c67cff3659cc6f5cfc
Lincoln Lee <git@lincoln.hk>
2017-09-19 06:59:38 +0800

    [Updating git-commit version: 20170914.258][removing old version: 20170609.2310]

--- elpa/git-commit-20170914.258/git-commit-autoloads.el
+++ elpa/git-commit-20170914.258/git-commit-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "git-commit" "git-commit.el" (22887 32403 0
+;;;### (autoloads nil "git-commit" "git-commit.el" (22976 20405 0
 ;;;;;;  0))
 ;;; Generated autoloads from git-commit.el
 
@@ -26,7 +26,7 @@ provide such a commit message.
 
 \(fn &optional ARG)" t nil)
 
-(defconst git-commit-filename-regexp "/\\(\\(\\(COMMIT\\|NOTES\\|PULLREQ\\|TAG\\)_EDIT\\|MERGE_\\|\\)MSG\\|BRANCH_DESCRIPTION\\)\\'")
+(defconst git-commit-filename-regexp "/\\(\\(\\(COMMIT\\|NOTES\\|PULLREQ\\|TAG\\)_EDIT\\|MERGE_\\|\\)MSG\\|\\(BRANCH\\|EDIT\\)_DESCRIPTION\\)\\'")
 
 (autoload 'git-commit-setup "git-commit" "\
 
@@ -35,7 +35,7 @@ provide such a commit message.
 
 ;;;***
 
-;;;### (autoloads nil nil ("git-commit-pkg.el") (22887 32369 0 0))
+;;;### (autoloads nil nil ("git-commit-pkg.el") (22976 20394 0 0))
 
 ;;;***
 
