82a7c5cd4b09a5a84fe110785e46ba7a724b6444
Lincoln Lee <git@lincoln.hk>
2017-11-13 19:32:58 +0800

    [Updating use-package version: 20171030.1428][removing old version: 20170812.2256]

--- elpa/use-package-20171030.1428/use-package-autoloads.el
+++ elpa/use-package-20171030.1428/use-package-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "use-package" "use-package.el" (22934 54062
+;;;### (autoloads nil "use-package" "use-package.el" (23049 33389
 ;;;;;;  0 0))
 ;;; Generated autoloads from use-package.el
 
