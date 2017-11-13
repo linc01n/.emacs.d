d565b8e0dd997f267856bd69996e0a5f7e21d5f4
Lincoln Lee <git@lincoln.hk>
2017-11-13 19:32:56 +0800

    [Updating nlinum version: 1.8.1][removing old version: 1.7]

--- elpa/nlinum-1.8.1/nlinum-autoloads.el
+++ elpa/nlinum-1.8.1/nlinum-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "nlinum" "nlinum.el" (22887 32356 0 0))
+;;;### (autoloads nil "nlinum" "nlinum.el" (23049 33408 0 0))
 ;;; Generated autoloads from nlinum.el
 
 (autoload 'nlinum-mode "nlinum" "\
