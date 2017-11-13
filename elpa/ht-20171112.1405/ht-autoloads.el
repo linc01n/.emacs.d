d2fc88f500ac2e2535df8965ad049f3a566285c2
Lincoln Lee <git@lincoln.hk>
2017-11-13 19:32:55 +0800

    [Updating ht version: 20171112.1405][removing old version: 20161015.1945]

--- elpa/ht-20171112.1405/ht-autoloads.el
+++ elpa/ht-20171112.1405/ht-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil nil ("ht.el") (22887 32394 0 0))
+;;;### (autoloads nil nil ("ht.el") (23049 33429 0 0))
 
 ;;;***
 
