960f8aeff1bb7c7591978a1fc0a6efd1d915b4b1
Lincoln Lee <git@lincoln.hk>
2017-11-13 19:32:55 +0800

    [Updating f version: 20171107.1248][removing old version: 20170404.1039]

--- elpa/f-20171107.1248/f-autoloads.el
+++ elpa/f-20171107.1248/f-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil nil ("f.el") (22887 32409 0 0))
+;;;### (autoloads nil nil ("f.el") (23049 33432 0 0))
 
 ;;;***
 
