e263c72130c83bb459da20d9e25aedc6b6e71948
Lincoln Lee <git@lincoln.hk>
2017-11-13 19:32:56 +0800

    [Updating ido-ubiquitous version: 20170923.842][removing old version: 20170819.1045]

--- elpa/ido-ubiquitous-20170923.842/ido-ubiquitous-autoloads.el
+++ elpa/ido-ubiquitous-20170923.842/ido-ubiquitous-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil nil ("ido-ubiquitous.el") (22976 20402 0 0))
+;;;### (autoloads nil nil ("ido-ubiquitous.el") (22995 36393 0 0))
 
 ;;;***
 
