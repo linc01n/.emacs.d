74182ba6450c872a0336a40031fb6e02edb290fc
Lincoln Lee <git@lincoln.hk>
2019-03-23 19:31:02 +0800

    [Updating org-bullets version: 20180208.2343][removing old version: 20140918.1137]

--- elpa/org-bullets-20180208.2343/org-bullets-autoloads.el
+++ elpa/org-bullets-20180208.2343/org-bullets-autoloads.el
@@ -1,16 +1,20 @@
 ;;; org-bullets-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
 
-;;;### (autoloads nil "org-bullets" "org-bullets.el" (22323 23606
-;;;;;;  0 0))
+;;;### (autoloads nil "org-bullets" "org-bullets.el" (0 0 0 0))
 ;;; Generated autoloads from org-bullets.el
 
 (autoload 'org-bullets-mode "org-bullets" "\
-UTF8 Bullets for org-mode
+Use UTF8 bullets in Org mode headings.
 
 \(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "org-bullets" '("org-bullets-")))
 
 ;;;***
 
@@ -18,5 +22,6 @@ UTF8 Bullets for org-mode
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
+;; coding: utf-8
 ;; End:
 ;;; org-bullets-autoloads.el ends here
