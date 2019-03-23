ab7cfafdd6071d00996338a693aa05c9f8dddde5
Lincoln Lee <git@lincoln.hk>
2019-03-23 19:31:04 +0800

    [Updating string-edit version: 20160411.656][removing old version: 20160410.2356]

--- elpa/string-edit-20160411.656/string-edit-autoloads.el
+++ elpa/string-edit-20160411.656/string-edit-autoloads.el
@@ -1,10 +1,12 @@
 ;;; string-edit-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
 
-;;;### (autoloads nil "string-edit" "string-edit.el" (22322 39106
-;;;;;;  0 0))
+;;;### (autoloads nil "string-edit" "string-edit.el" (0 0 0 0))
 ;;; Generated autoloads from string-edit.el
 
 (autoload 'string-edit-at-point "string-edit" "\
@@ -13,11 +15,14 @@ This saves you from needing to manually escape characters.
 
 \(fn)" t nil)
 
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "string-edit" '("string-edit-" "se/")))
+
 ;;;***
 
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
+;; coding: utf-8
 ;; End:
 ;;; string-edit-autoloads.el ends here
