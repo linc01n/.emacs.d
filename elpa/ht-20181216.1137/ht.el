5a579a14393db3de511846aedc2bbf2d80793729
Lincoln Lee <git@lincoln.hk>
2019-03-23 19:31:00 +0800

    [Updating ht version: 20181216.1137][removing old version: 20171112.1405]

--- elpa/ht-20181216.1137/ht.el
+++ elpa/ht-20181216.1137/ht.el
@@ -4,7 +4,7 @@
 
 ;; Author: Wilfred Hughes <me@wilfred.me.uk>
 ;; Version: 2.3
-;; Package-Version: 20171112.1405
+;; Package-Version: 20181216.1137
 ;; Keywords: hash table, hash map, hash
 ;; Package-Requires: ((dash "2.12.0"))
 
@@ -30,6 +30,7 @@
 ;;; Code:
 
 (require 'dash)
+(require 'gv)
 
 (defmacro ht (&rest pairs)
   "Create a hash table with the key-value pairs given.
@@ -44,6 +45,13 @@ Keys are compared with `equal'.
     `(let ((,table-symbol (ht-create)))
        ,@assignments
        ,table-symbol)))
+
+(defsubst ht-set! (table key value)
+  "Associate KEY in TABLE with VALUE."
+  (puthash key value table)
+  nil)
+
+(defalias 'ht-set 'ht-set!)
 
 (defsubst ht-create (&optional test)
   "Create an empty hash table.
@@ -76,7 +84,7 @@ TEST indicates the function used to compare the hash
 keys.  Default is `equal'.  It can be `eq', `eql', `equal' or a
 user-supplied test created via `define-hash-table-test'."
   (let ((h (ht-create test)))
-    (dolist (pair (-partition 2 plist) h)
+    (dolist (pair (nreverse (-partition 2 plist)) h)
       (let ((key (car pair))
             (value (cadr pair)))
         (ht-set! h key value)))))
@@ -88,20 +96,26 @@ user-supplied test created via `define-hash-table-test'."
 If KEY isn't present, return DEFAULT (nil if not specified)."
   (gethash key table default))
 
+(gv-define-setter ht-get (value table key) `(ht-set! ,table ,key ,value))
+
 (defun ht-get* (table &rest keys)
   "Look up KEYS in nested hash tables, starting with TABLE.
 The lookup for each key should return another hash table, except
 for the final key, which may return any value."
   (if (cdr keys)
       (apply #'ht-get* (ht-get table (car keys)) (cdr keys))
-    (ht-get table (car keys))))
+    (if keys
+        (ht-get table (car keys))
+      table)))
 
-(defsubst ht-set! (table key value)
-  "Associate KEY in TABLE with VALUE."
-  (puthash key value table)
-  nil)
-
-(defalias 'ht-set 'ht-set!)
+(gv-define-setter ht-get* (value table &rest keys)
+  `(if (cdr ',keys)
+       (let* ((first-key (car ',keys))
+              (last-key (-last-item ',keys))
+              (butlast-key (butlast (cdr ',keys)))
+              (h (apply #'ht-get* (ht-get ,table first-key) butlast-key)))
+         (ht-set! h last-key ,value))
+     (ht-set! ,table (car ',keys) ,value)))
 
 (defun ht-update! (table from-table)
   "Update TABLE according to every key-value pair in FROM-TABLE."
