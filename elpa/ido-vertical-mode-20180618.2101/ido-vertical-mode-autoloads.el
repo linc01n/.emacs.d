dce947bfe78331312a699382f5e61b6e1bd9cf0e
Lincoln Lee <git@lincoln.hk>
2019-03-23 19:31:00 +0800

    [Updating ido-vertical-mode version: 20180618.2101][removing old version: 20160429.1037]

--- elpa/ido-vertical-mode-20180618.2101/ido-vertical-mode-autoloads.el
+++ elpa/ido-vertical-mode-20180618.2101/ido-vertical-mode-autoloads.el
@@ -1,15 +1,19 @@
 ;;; ido-vertical-mode-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
 
 ;;;### (autoloads nil "ido-vertical-mode" "ido-vertical-mode.el"
-;;;;;;  (22322 39065 0 0))
+;;;;;;  (0 0 0 0))
 ;;; Generated autoloads from ido-vertical-mode.el
 
 (defvar ido-vertical-mode nil "\
 Non-nil if Ido-Vertical mode is enabled.
-See the command `ido-vertical-mode' for a description of this minor mode.
+See the `ido-vertical-mode' command
+for a description of this minor mode.
 Setting this variable directly does not take effect;
 either customize it (see the info node `Easy Customization')
 or call the function `ido-vertical-mode'.")
@@ -21,11 +25,14 @@ Makes ido-mode display vertically.
 
 \(fn &optional ARG)" t nil)
 
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ido-vertical-mode" '("ido-vertical-" "turn-o")))
+
 ;;;***
 
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
+;; coding: utf-8
 ;; End:
 ;;; ido-vertical-mode-autoloads.el ends here
