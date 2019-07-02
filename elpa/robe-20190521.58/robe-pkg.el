29bdce92230e2cbfca354b9e935c60ad9dce097a
Lincoln Lee <git@lincoln.hk>
2019-07-02 22:46:27 +0800

    [Updating robe version: 20190521.58][removing old version: 20190425.125]

--- elpa/robe-20190521.58/robe-pkg.el
+++ elpa/robe-20190521.58/robe-pkg.el
@@ -1,4 +1,4 @@
-(define-package "robe" "20190425.125" "Code navigation, documentation lookup and completion for Ruby"
+(define-package "robe" "20190521.58" "Code navigation, documentation lookup and completion for Ruby"
   '((inf-ruby "2.5.1")
     (emacs "24.4"))
   :keywords
