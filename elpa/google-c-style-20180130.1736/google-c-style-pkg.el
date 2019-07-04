a63c34c040556fe7c1f006cb4d03a74e4dc34076
Lincoln Lee <git@lincoln.hk>
2019-07-04 22:08:39 +0800

    [Adding google-c-style version: 20180130.1736]

---
+++ elpa/google-c-style-20180130.1736/google-c-style-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "google-c-style" "20180130.1736" "Google's C/C++ style for c-mode" 'nil :commit "9c6b371f419b1dd33c338846e3e7c1ce7304baff" :keywords '("c" "tools"))
