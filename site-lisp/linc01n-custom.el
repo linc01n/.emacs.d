717cb10c241ce2d6aabd7289453349060cbc62ba
Lincoln Lee <git@lincoln.hk>
2019-11-13 17:54:37 +0800

    Add custom variable for org

--- site-lisp/linc01n-custom.el
+++ site-lisp/linc01n-custom.el
@@ -5,9 +5,12 @@
  ;; If there is more than one, they won't work right.
  '(blink-cursor-mode nil)
  '(current-language-environment "UTF-8")
+ '(org-standup-title-format "Lincoln %F")
+ '(org-startup-folded nil)
+ '(org-startup-truncated nil)
  '(package-selected-packages
    (quote
-    (nix-mode zenburn-theme yasnippet yaml-mode whitespace-cleanup-mode which-key wgrep-ag vlf visual-regexp-steroids tabbar switch-window swiper string-edit spaceline smooth-scrolling smex smartparens rvm robe req-package rainbow-delimiters projectile-rails php-mode ox-gfm osx-plist org-bullets nlinum neotree multiple-cursors move-text markdown-mode magit-popup magit load-dir jsx-mode json-mode js2-mode insert-shebang indent-guide iedit ido-vertical-mode ido-ubiquitous haml-mode golden-ratio go-eldoc git-timemachine git-gutter-fringe flyspell-popup flycheck-gometalinter flx-ido expand-region exec-path-from-shell evil el-get edit-server discover-my-major dired+ diminish company-go coffee-mode avy anzu aggressive-indent ag)))
+    (org-standup nix-mode zenburn-theme yasnippet yaml-mode whitespace-cleanup-mode which-key wgrep-ag vlf visual-regexp-steroids tabbar switch-window swiper string-edit spaceline smooth-scrolling smex smartparens rvm robe req-package rainbow-delimiters projectile-rails php-mode ox-gfm osx-plist org-bullets nlinum neotree multiple-cursors move-text markdown-mode magit-popup magit load-dir jsx-mode json-mode js2-mode insert-shebang indent-guide iedit ido-vertical-mode ido-ubiquitous haml-mode golden-ratio go-eldoc git-timemachine git-gutter-fringe flyspell-popup flycheck-gometalinter flx-ido expand-region exec-path-from-shell evil el-get edit-server discover-my-major dired+ diminish company-go coffee-mode avy anzu aggressive-indent ag)))
  '(tab-width 2)
  '(tabbar-separator (quote (0.5))))
 (setq js-indent-level 2)
