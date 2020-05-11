39bd03730ab7249c85b6a447571d998ae030615a
Lincoln Lee <git@lincoln.hk>
2020-05-11 21:29:12 +0800

    [Updating vlf version: 20191126.2250][removing old version: 20180201.2254]

--- elpa/vlf-20191126.2250/vlf-pkg.el
+++ elpa/vlf-20191126.2250/vlf-pkg.el
@@ -1,4 +1,4 @@
-(define-package "vlf" "20180201.2254" "View Large Files" 'nil :keywords
+(define-package "vlf" "20191126.2250" "View Large Files" 'nil :keywords
   '("large files" "utilities")
   :maintainer
   '("Andrey Kotlarski" . "m00naticus@gmail.com")
