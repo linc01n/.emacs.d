855b949669e05d23a43d426744d01b1ba11c5bb0
Lincoln Lee <git@lincoln.hk>
2020-06-06 21:32:21 +0800

    [Updating nix-mode version: 20200521.1745][removing old version: 20200510.212]

--- elpa/nix-mode-20200521.1745/nix-mode-pkg.el
+++ elpa/nix-mode-20200521.1745/nix-mode-pkg.el
@@ -1,6 +1,6 @@
-(define-package "nix-mode" "20200510.212" "Major mode for editing .nix files"
+(define-package "nix-mode" "20200521.1745" "Major mode for editing .nix files"
   '((emacs "25"))
-  :keywords
+  :commit "71fda1db41160e03b181ea00086a869ae93546e1" :keywords
   '("nix" "languages" "tools" "unix")
   :maintainer
   '("Matthew Bauer" . "mjbauer95@gmail.com")
