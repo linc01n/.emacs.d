5a4baa2f423e4458a531bb54112dbab644c477ba
Lincoln Lee <git@lincoln.hk>
2020-06-06 21:32:21 +0800

    [Updating js2-mode version: 20200601.2018][removing old version: 20200427.33]

--- elpa/js2-mode-20200601.2018/js2-mode-pkg.el
+++ elpa/js2-mode-20200601.2018/js2-mode-pkg.el
@@ -1,7 +1,7 @@
-(define-package "js2-mode" "20200427.33" "Improved JavaScript editing mode"
+(define-package "js2-mode" "20200601.2018" "Improved JavaScript editing mode"
   '((emacs "24.1")
     (cl-lib "0.5"))
-  :keywords
+  :commit "5049e543b52099e6ea3e9bc915fc023d5a9b2644" :keywords
   '("languages" "javascript")
   :authors
   '(("Steve Yegge" . "steve.yegge@gmail.com")
