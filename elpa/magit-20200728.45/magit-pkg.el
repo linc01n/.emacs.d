eb200fa07370b466b1904f1a5b7b814cbc2166c1
Lincoln Lee <git@lincoln.hk>
2020-07-31 23:32:03 +0800

    [Updating magit version: 20200728.45][removing old version: 20200706.1352]

--- elpa/magit-20200728.45/magit-pkg.el
+++ elpa/magit-20200728.45/magit-pkg.el
@@ -1,11 +1,11 @@
-(define-package "magit" "20200706.1352" "A Git porcelain inside Emacs."
+(define-package "magit" "20200728.45" "A Git porcelain inside Emacs."
   '((emacs "25.1")
     (async "20200113")
     (dash "20200524")
     (git-commit "20200516")
     (transient "20200601")
     (with-editor "20200522"))
-  :commit "7d4bcd72d478f62015d45a24a4a7419135a14cdf" :keywords
+  :commit "fccd3cff52fc5b655bbb80bc6c894a2a27f188ea" :keywords
   '("git" "tools" "vc"))
 ;; Local Variables:
 ;; no-byte-compile: t
