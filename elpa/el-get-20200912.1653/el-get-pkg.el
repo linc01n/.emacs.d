80a929b9faf7dbabd952d04398dbf772f6f4031a
Lincoln Lee <git@lincoln.hk>
2020-09-19 14:55:52 +0800

    [Updating el-get version: 20200912.1653][removing old version: 20181006.225]

--- elpa/el-get-20200912.1653/el-get-pkg.el
+++ elpa/el-get-20200912.1653/el-get-pkg.el
@@ -1,4 +1,4 @@
-(define-package "el-get" "20181006.225" "Manage the external elisp bits and pieces you depend upon" 'nil :keywords
+(define-package "el-get" "20200912.1653" "Manage the external elisp bits and pieces you depend upon" 'nil :commit "6138ce5b0a9df779e33ef42eadc4c15a5cf65f39" :keywords
   '("emacs" "package" "elisp" "install" "elpa" "git" "git-svn" "bzr" "cvs" "svn" "darcs" "hg" "apt-get" "fink" "pacman" "http" "http-tar" "emacswiki")
   :authors
   '(("Dimitri Fontaine" . "dim@tapoueh.org"))
