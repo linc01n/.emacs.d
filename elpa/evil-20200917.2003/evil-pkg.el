3586e7d6e089667edfaec71e408fae809feb0273
Lincoln Lee <git@lincoln.hk>
2020-09-19 14:55:52 +0800

    [Updating evil version: 20200917.2003][removing old version: 20200718.842]

--- elpa/evil-20200917.2003/evil-pkg.el
+++ elpa/evil-20200917.2003/evil-pkg.el
@@ -1,9 +1,9 @@
-(define-package "evil" "20200718.842" "Extensible Vi layer for Emacs."
+(define-package "evil" "20200917.2003" "Extensible Vi layer for Emacs."
   '((emacs "24.1")
     (undo-tree "0.7.4")
     (goto-chg "1.6")
     (cl-lib "0.5"))
-  :commit "2bc6ca3587502fde67b93e411e329fc8224c916a")
+  :commit "3277bf66fff1fac7ff58a046de3d42c31375fdb2")
 ;; Local Variables:
 ;; no-byte-compile: t
 ;; End:
